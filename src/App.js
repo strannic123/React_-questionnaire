f55f61045fb7dac816581dd2037657d57f70e541
str <manager350@mail.ru>
2020-05-19 20:27:25 +0800

    Added navigation. Registration / test list / authorization pages added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import Layout from "./hoc/Layout/Layout";
+import {Route, Switch} from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
+import QuizList from "./containers/QuizList/QuizList";
+import Auth from "./containers/Auth/Auth";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+
 
 
 // function App() {
@@ -14,7 +19,12 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Switch>
+          <Route path="/auth" component={Auth}/>
+          <Route path="/quiz-creator" component={QuizCreator}/>
+          <Route path="/quiz/:id" component={Quiz}/>
+          <Route path="/" component={QuizList}/>
+        </Switch>
       </Layout>
     )
   }
--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './FinishedQuiz.css'
 import Button from "../UI/Button/Button";
+import {Link} from 'react-router-dom'
 
 const FinishedQuiz = props => {
   const succesCount = Object.keys(props.results).reduce((total, key) => {
@@ -44,7 +45,10 @@ const FinishedQuiz = props => {
 
       <div>
         <Button onClick={props.onRetry} type='primary'>Повторить</Button>
-        <Button onClick={props.onRetry} type='success'>Перейти в список тестов</Button>
+        <Link to='/'>
+          <Button onClick={props.onRetry} type='success'>Перейти в список тестов</Button>
+        </Link>
+
       </div>
     </div>
   )
